7202dc2094ae7574e220eceba240b3716e8831c0
huseyin <huseyinarslan06@gmail.com>
2022-07-06 17:58:21 +0300

    paginate

--- src/components/Followers.jsx
+++ src/components/Followers.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import Paginate from "./Paginate";
+import CardFollowers from "./CardFollowers";
+import loadingGif from "../assets/loading.gif";
+import { useState } from "react";
+import { Container, Row } from "react-bootstrap";
 
-const Followers = () => {
+const Followers = ({ followers }) => {
+  const { loading, followersList } = followers;
+  const [followersPerPAge] = useState(12);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastFollower = currentPage * followersPerPAge;
+  const indexOfFirstFollower = indexOfLastFollower - followersPerPAge;
+
+  const currentFollowers = followersList.slice(
+    indexOfFirstFollower,
+    indexOfLastFollower
+  );
+
+  const totalPages = Math.ceil(followersList.length / followersPerPAge);
+
   return (
-    <div>Followers</div>
-  )
-}
+    <div>
+      {loading ? (
+        <div>
+          <img src={loadingGif} alt="loading..." />
+        </div>
+      ) : (
+        <div>
+          <Container>
+            <Row xs={2} md={3} lg={4}>
+              {currentFollowers?.map((follower, index) => (
+                <div key={index}>
+                  <CardFollowers follower={follower} />
+                </div>
+              ))}
+            </Row>
+          </Container>
+          <div>
+            <Paginate pages={totalPages} setCurrentPage={setCurrentPage} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Followers
+export default Followers;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import axios from "axios";
+import SearchUser from "../components/SearchUser";
+import Followers from "../components/Followers";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [allFollowers, setAllFollowers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
+
+  const getFollowers = async () => {
+    const { data } = await axios.get(
+      "https://api.github.com/users/anthonyharold67/followers?per_page=100"
+    );
+    setAllFollowers(data);
+    console.log(data);
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    getFollowers();
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  const followersList = allFollowers.filter((follower) =>
+    follower.login.includes(search)
+  );
+
+  return (
+    <div>
+      <SearchUser handleChange={handleChange} />
+      <Followers followers={{ loading, followersList }} />
+    </div>
+  );
 };
 
 export default Home;
